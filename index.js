740532bc6c2fd6b250b96a363a128d439e3b01e0
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-04 11:15:39 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+
+const newHeader = document.createElement("h1")
+
+
+newHeader.setAttribute("id" , "victory");
+newHeader.innerHTML = "Stacy Joy is the champion"
+
+const body = document.getElementsByTagName("body")[0];
+body.appendChild(newHeader)
